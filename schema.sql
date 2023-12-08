1f215c000cb0bf555c591121c70d626e37282e5b
Celia_WAJ <65226781+celiawaj@users.noreply.github.com>
2023-12-07 19:01:39 -0500

    Add data to database.

--- schema.sql
+++ schema.sql
@@ -1,34 +1,37 @@
-
+DROP TABLE IF EXISTS zipcodes, complaints, trees, rents;
 CREATE TABLE zipcodes
 (
     zipcode TEXT PRIMARY KEY,
     county TEXT,
-    geometry GEOMETRY(Point)
-)
+    geometry GEOMETRY(Polygon, 4326)
+);
+CREATE INDEX zipcodes_geom_idx ON zipcodes USING GIST (geometry);
 
-CREATE TABLE nyc_311
+CREATE TABLE complaints
 (
     id INTEGER PRIMARY KEY,
     zipcode TEXT,
     date TIMESTAMP,
     complaint_type TEXT,
-    geometry GEOMETRY(Point)
-)
+    geometry GEOMETRY(Polygon, 4326)
+);
+CREATE INDEX complaints_geom_idx ON complaints USING GIST (geometry);
 
-CREATE TABLE nyc_tree
+CREATE TABLE trees
 (
     id INTEGER PRIMARY KEY,
     zipcode TEXT,
     species TEXT,
     health TEXT,
     status TEXT,
-    geometry GEOMETRY(Point)
-)
+    geometry GEOMETRY(Polygon, 4326)
+);
+CREATE INDEX trees_geom_idx ON trees USING GIST (geometry);
 
-CREATE TABLE zillow
+CREATE TABLE rents
 (
     id INTEGER PRIMARY KEY,
     zipcode TEXT,
     aug_rent FLOAT,
     sep_rent FLOAT
-)
+);
