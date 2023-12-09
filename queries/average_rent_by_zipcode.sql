d824be14e7782d51f7b01d061299b75579eaba05
Xiaochen Yan <xy2612@columbia.edu>
2023-12-08 19:21:04 -0500

    Visualization 2, 4, and 6.

--- queries/average_rent_by_zipcode.sql
+++ queries/average_rent_by_zipcode.sql
@@ -1,7 +1,7 @@
 
 SELECT trees.zipcode, TO_CHAR(AVG(rents.aug_rent), 'FM9,999,999.00') AS august_rent
 FROM trees
-LEFT OUTER JOIN rents ON trees.zipcode = rents.zipcode
+JOIN rents ON trees.zipcode = rents.zipcode
 GROUP BY trees.zipcode
 ORDER BY COUNT(*) DESC
 LIMIT 10;
